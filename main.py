db23106705985121def3e048f40315cb23e9300d
alec <azadikian1@hwemail.com>
2016-10-27 18:40:43 -0700

    changed loss model, still a bug I think

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ KEY_VOLUME = 1
 KEY_OPEN = 2
 KEY_HIGH = 3
 KEY_LOW = 4
-DENOM=1000
+MAX_LOSS_OVERRIDE = None
 
 class MWU:
 
-    def __init__(self, num_experts, epsilon=0.5):
+    def __init__(self, num_experts, epsilon=0.05):
         self._weights = [1.0] * num_experts
         self._epsilon = epsilon
         self._loss = 0
         self._losses = [0.0] * num_experts
 
-    def run_iteration(self, expert_losses):
+    def run_iteration(self, expert_losses, max_loss):
         for i, loss in enumerate(expert_losses):
             prob = self._weights[i] / sum(self._weights)
             e_loss = loss * prob
@@ -37,7 +37,7 @@ class MWU:
                 error("invalid loss detected")
             self._loss += e_loss # TODO IDK about this...
             self._losses[i] += e_loss
-            self._weights[i] = self._weights[i] * pow((1 - self._epsilon), loss)
+            self._weights[i] = self._weights[i] * pow((1 - self._epsilon), loss / max_loss)
 
     def get_loss(self):
         return self._loss
@@ -48,7 +48,7 @@ class MWU:
     def get_weights(self):
         return self._weights
 
-def load_data(dir=DATA_DIR):
+def load_data(dir=DATA_DIR, short=False, whitelist=None):
     stocks = OrderedDict() # stock: tuple
     dates = set()
 
@@ -96,7 +96,7 @@ def load_data(dir=DATA_DIR):
             yield date.strftime("%Y/%m/%d"), r_close, 0, 0, 0, 0
 
     for fname in os.listdir(dir):
-        if not fname.endswith(".csv"):
+        if not fname.endswith(".csv") or (whitelist is not None and whitelist not in fname):
             continue
 
         verbose("Opening file {}... ".format(fname), end="")
@@ -104,11 +104,12 @@ def load_data(dir=DATA_DIR):
 
         symbol = fname.split(".")[0]
         stocks[symbol] = date_price
-        stocks["-" + symbol] = short_date_price
+        if short:
+            stocks["-" + symbol] = short_date_price
         verbose("Done. Loaded {} dates.".format(len(date_price)))
 
     sorted_dates = sorted(dates)
-    for fake_i in range(0, 1):
+    for fake_i in range(0, 0): # to add fake stocks
 
         def mk_trending(rate, dev=0.25):
             def trending(date):
@@ -119,15 +120,24 @@ def load_data(dir=DATA_DIR):
 
         date_price, short_date_price = load_rows(fake_rows(mk_trending(-1, dev=0.25), sorted_dates))
         stocks["fake{}".format(fake_i)] = date_price
-        stocks["-fake{}".format(fake_i)] = short_date_price
+        if short:
+            stocks["-fake{}".format(fake_i)] = short_date_price
 
     return (stocks, sorted(dates))
 
-def run_mwu(start_money=1.0):
+def run_mwu():
 
     stocks, dates = load_data()
     mwu = MWU(len(stocks))
     last_close = [None] * len(stocks)
+
+    start_money = 0.0
+    first_date = dates[0]
+    for i, tup in enumerate(stocks.items()):
+        stock, date_price = tup
+        price = date_price[first_date]
+        cls, vol, op, hi, lo = price[KEY_CLOSE], price[KEY_VOLUME], price[KEY_OPEN], price[KEY_HIGH], price[KEY_LOW]
+        start_money += math.fabs(cls)
 
     money = start_money
     iteration = 0
@@ -139,8 +149,7 @@ def run_mwu(start_money=1.0):
             cls, vol, op, hi, lo = price[KEY_CLOSE], price[KEY_VOLUME], price[KEY_OPEN], price[KEY_HIGH], price[KEY_LOW]
 
             if last_close[i] is not None:
-                abs_loss = last_close[i] - cls
-                loss = abs_loss / DENOM
+                loss = last_close[i] - cls
                 if not math.isfinite(loss):
                     error("invalid loss detected")
                 losses.append(loss)
@@ -151,10 +160,10 @@ def run_mwu(start_money=1.0):
         weights = mwu.get_weights()
         for i, loss in enumerate(losses):
             # We're going to say that every day, we sell everything then buy everything according to weights
-            iter_loss = loss * weights[i] / sum(weights)
+            iter_loss = loss * weights[i] / sum(weights) * money / start_money
         money -= iter_loss
 
-        mwu.run_iteration(losses)
+        mwu.run_iteration(losses, max_loss=money)
         iteration += 1
         if iteration % 100 == 0:
             verbose("{} iterations done".format(iteration), end="\r")
